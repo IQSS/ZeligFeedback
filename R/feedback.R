144f762729946b0064708a9c40c42753ba8ecc44
cchoirat <cchoirat@gmail.com>
2015-03-09 13:32:32 -0400

    First commit

---
+++ R/feedback.R
@@ -0,0 +1,4 @@
+feedback <- function(zobj) {
+  # We can use methods and field from the Zelig object zobj
+  return(zobj$name)
+}
---
+++ demo/demo-feedback.R
@@ -0,0 +1,3 @@
+# library(Zelig)
+# z5 <- zlogit$new()
+# z5$feedback()
